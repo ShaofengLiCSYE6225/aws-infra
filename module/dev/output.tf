f592f14cb5e7c707c607dcf6381e8e03c57d565b
shaofeng li <2604566769@qq.com>
2023-03-02 12:53:05 -0500

    first

--- module/dev/output.tf
+++ module/dev/output.tf
@@ -1,6 +1,9 @@
 output "public_subnet" {
     value = aws_subnet.public_subnet
 }
+output "private_subnet" {
+  value = aws_subnet.private_subnet
+}
 output "vpc_id" {
   value = aws_vpc.dev.id
 }
--- var.tfvars
+++ var.tfvars
@@ -1,7 +1,7 @@
-profile = "prod"
+profile = "dev"
 
 region = "us-east-1"
 
 cidr = "10.0.0.0/16"
 
-key_pair_id = "key-08dff20332760a94d"
+ami_id = "ami-0e769e709618b6b87"
