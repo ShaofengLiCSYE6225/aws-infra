a7b4e7a8808cfad895d623188db996d8b67e5a06
shaofeng li <2604566769@qq.com>
2023-04-05 21:45:09 -0400

    first commit

--- main.tf
+++ main.tf
@@ -7,12 +7,137 @@ data "aws_ami" "webapp_ami" {
   most_recent = true
 }
 
+data "template_file" "user_data" {
+  #!/bin/bash
+  template = <<EOF
+  #!/bin/bash
+  echo "DATABASE_HOST=${replace(aws_db_instance.csye6225.endpoint, "/:.*/", "")}" >> /home/ec2-user/.env
+  echo "DATABASE_NAME=${aws_db_instance.csye6225.db_name}" >> /home/ec2-user/.env
+  echo "DATABASE_USERNAME=${aws_db_instance.csye6225.username}" >> /home/ec2-user/.env
+  echo "DATABASE_PASSWORD=${aws_db_instance.csye6225.password}" >> /home/ec2-user/.env
+  echo "DIALECT=${aws_db_instance.csye6225.engine}" >> /home/ec2-user/.env 
+  echo "AWS_BUCKET_NAME=${aws_s3_bucket.bucket.bucket}" >> /home/ec2-user/.env 
+  echo "AWS_BUCKET_REGION=${var.region}" >> /home/ec2-user/.env 
+  mv /home/ec2-user/.env /home/ec2-user/webapp/.env
+  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/tmp/cloudwatchconfig.json \
+    -s
+  EOF
+}
+resource "aws_lb_target_group" "alb_tg" {
+  name        = "csye6225-lb-alb-tg"
+  target_type = "instance"
+  port        = 3000
+  protocol = "HTTP"
+  vpc_id = module.dev.vpc_id
+  health_check {
+    port     = 3000
+    protocol = "HTTP"
+    path     = "/healthz"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+
+  }
+
+}
+resource "aws_launch_template" "lt" {
+  name          = "asg_launch_config"
+  image_id      = var.ami_id
+  instance_type = "t2.micro"
+  key_name      = "ec2"
+  iam_instance_profile {
+    name = aws_iam_instance_profile.CSYE6225.name
+  }
+  network_interfaces {
+    security_groups             = [aws_security_group.web_sg.id]
+    associate_public_ip_address = true
+  }
+  user_data = base64encode(data.template_file.user_data.rendered)
+
+}
+resource "aws_autoscaling_group" "asg" {
+  name             = "csye6225-asg-spring2023"
+  default_cooldown = 60
+  max_size         = 3
+  min_size         = 1
+  desired_capacity = 1
+vpc_zone_identifier = module.dev.public_subnet.*.id 
+target_group_arns = [aws_lb_target_group.alb_tg.arn]
+  launch_template {
+    id      = aws_launch_template.lt.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    value               = "asg_launch_config"
+    propagate_at_launch = true
+  }
+}
+resource "aws_autoscaling_policy" "scale_up" {
+  name                   = "csye6225-asg-cpu-scale-up"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  adjustment_type        = "ChangeInCapacity"
+  policy_type            = "SimpleScaling"
+  scaling_adjustment     = 1
+}
+resource "aws_autoscaling_policy" "scale_down" {
+  name                   = "csye6225-asg-cpu-scale-down"
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  adjustment_type        = "ChangeInCapacity"
+  policy_type            = "SimpleScaling"
+  scaling_adjustment     = -1
+}
+resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
+  alarm_name          = "csye6225-asg-scale-up-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  period              = 60
+  namespace           = "AWS/EC2"
+  statistic           = "Average"
+  threshold           = 5
+  alarm_description   = "This metric monitors ec2 cpu utilization"
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.scale_up.arn]
+}
+resource "aws_cloudwatch_metric_alarm" "scale_down_alarm" {
+  alarm_name          = "csye6225-asg-scale-down-alarm"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  period              = 60
+  namespace           = "AWS/EC2"
+  statistic           = "Average"
+  threshold           = 3
+  alarm_description   = "This metric monitors ec2 cpu utilization"
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.scale_down.arn]
+}
 resource "aws_route53_record" "dev" {
   zone_id = var.route_zone_id
   name    = var.route_name
-  ttl     = 60
   type    = "A"
-  records = [aws_instance.ec2.public_ip]
+  alias {
+    name                   = aws_lb.load_balancer.dns_name
+    zone_id                = aws_lb.load_balancer.zone_id
+    evaluate_target_health = true
+  }
 }
 resource "aws_iam_instance_profile" "CSYE6225" {
   name = "instance_profile"
@@ -86,11 +211,10 @@ resource "aws_security_group" "database" {
     security_groups = [aws_security_group.web_sg.id]
   }
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 resource "aws_s3_bucket" "bucket" {
@@ -154,29 +278,78 @@ resource "aws_security_group" "web_sg" {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.lb_sg.id]
   }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    security_groups = [aws_security_group.lb_sg.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+# resource "aws_instance" "ec2" {
+#   ami                         = var.ami_id
+#   instance_type               = "t2.micro"
+#   disable_api_termination     = false
+#   subnet_id                   = module.dev.public_subnet.*.id[0]
+#   vpc_security_group_ids      = [aws_security_group.web_sg.id]
+#   associate_public_ip_address = true
+#   disable_api_stop            = false
+#   key_name                    = var.key_name
+#   iam_instance_profile        = aws_iam_instance_profile.CSYE6225.name
+
+#   user_data = <<EOF
+#   #!/bin/bash
+#   echo "DATABASE_HOST=${replace(aws_db_instance.csye6225.endpoint, "/:.*/", "")}" >> /home/ec2-user/.env
+#   echo "DATABASE_NAME=${aws_db_instance.csye6225.db_name}" >> /home/ec2-user/.env
+#   echo "DATABASE_USERNAME=${aws_db_instance.csye6225.username}" >> /home/ec2-user/.env
+#   echo "DATABASE_PASSWORD=${aws_db_instance.csye6225.password}" >> /home/ec2-user/.env
+#   echo "DIALECT=${aws_db_instance.csye6225.engine}" >> /home/ec2-user/.env 
+#   echo "AWS_BUCKET_NAME=${aws_s3_bucket.bucket.bucket}" >> /home/ec2-user/.env 
+#   echo "AWS_BUCKET_REGION=${var.region}" >> /home/ec2-user/.env 
+#   mv /home/ec2-user/.env /home/ec2-user/webapp/.env
+#   sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+#     -a fetch-config \
+#     -m ec2 \
+#     -c file:/tmp/cloudwatchconfig.json \
+#     -s
+#   EOF
+#   root_block_device {
+#     volume_size = 50
+#     volume_type = "gp2"
+#   }
+
+# }
+resource "aws_lb" "load_balancer" {
+  name               = "csye6225-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = module.dev.public_subnet.*.id
+  tags = {
+    application : "Webapp"
+  }
+}
+resource "aws_security_group" "lb_sg" {
+  name        = "load_balancer_sg"
+  description = "lb security group"
+  vpc_id = module.dev.vpc_id
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
+ ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -186,38 +359,5 @@ resource "aws_security_group" "web_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-resource "aws_instance" "ec2" {
-  ami                         = var.ami_id
-  instance_type               = "t2.micro"
-  disable_api_termination     = false
-  subnet_id                   = module.dev.public_subnet.*.id[0]
-  vpc_security_group_ids      = [aws_security_group.web_sg.id]
-  associate_public_ip_address = true
-  disable_api_stop            = false
-  key_name                    = var.key_name
-  iam_instance_profile        = aws_iam_instance_profile.CSYE6225.name
-
-  user_data = <<EOF
-  #!/bin/bash
-  echo "DATABASE_HOST=${replace(aws_db_instance.csye6225.endpoint, "/:.*/", "")}" >> /home/ec2-user/.env
-  echo "DATABASE_NAME=${aws_db_instance.csye6225.db_name}" >> /home/ec2-user/.env
-  echo "DATABASE_USERNAME=${aws_db_instance.csye6225.username}" >> /home/ec2-user/.env
-  echo "DATABASE_PASSWORD=${aws_db_instance.csye6225.password}" >> /home/ec2-user/.env
-  echo "DIALECT=${aws_db_instance.csye6225.engine}" >> /home/ec2-user/.env 
-  echo "AWS_BUCKET_NAME=${aws_s3_bucket.bucket.bucket}" >> /home/ec2-user/.env 
-  echo "AWS_BUCKET_REGION=${var.region}" >> /home/ec2-user/.env 
-  mv /home/ec2-user/.env /home/ec2-user/webapp/.env
-  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file:/tmp/cloudwatchconfig.json \
-    -s
-  EOF
-  root_block_device {
-    volume_size = 50
-    volume_type = "gp2"
-  }
-
 }
 
--- var.tfvars
+++ var.tfvars
@@ -4,4 +4,4 @@ region = "us-east-1"
 
 cidr = "10.0.0.0/16"
 
-ami_id = "ami-08e1039822eed1fc3"
+ami_id = "ami-05fff052090a8dc26"
